74a2571eeb81c469309b3e48afc4e97099a2a2ca
bhuvitest <bhuvanesh.moorthy@testleaf.com>
2025-11-29 17:19:31 +0530

    Update ServiceNow URL and login credentials

--- src/main/java/bulidVersionOne/CredentialsCheck.java
+++ src/main/java/bulidVersionOne/CredentialsCheck.java
@@ -13,12 +13,12 @@ public class CredentialsCheck {
 
 		ChromeDriver driver = new ChromeDriver();
 		//1. Launch ServiceNow application
-		driver.get("https://dev273042.service-now.com/");
+		driver.get("https://dev300891.service-now.com/");
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
         //2. Login with valid credentials username as admin and password
 		driver.findElement(By.xpath("//input[@id='user_name']")).sendKeys("admin");
-		driver.findElement(By.xpath("//input[@id='user_password']")).sendKeys("*w6ndQP4vI$G");
+		driver.findElement(By.xpath("//input[@id='user_password']")).sendKeys("03-xMN$nqSmU");
 		WebElement login = driver.findElement(By.xpath("//button[text()='Log in']"));
 		login.click();
 		driver.quit();
