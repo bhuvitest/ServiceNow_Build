122d2d909a3b1031e4e2ca5c9e7b04ce76e2b25b
Bhuvanesh Moorthy <bhuvanesh.moorthy@testleaf.com>
2025-12-07 21:27:33 +0530

    --Bhuvanesh,Seeni credentials added--

--- src/main/java/bulidVersionOne/CredentialsCheck.java
+++ src/main/java/bulidVersionOne/CredentialsCheck.java
@@ -8,8 +8,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class CredentialsCheck {
-	@Test
-	public void runBuild() {
+	@Test(priority = 1)
+	public void runBuildBhuvanesh() {
 
 		ChromeDriver driver = new ChromeDriver();
 		//1. Launch ServiceNow application
@@ -19,6 +19,22 @@ public class CredentialsCheck {
         //2. Login with valid credentials username as admin and password
 		driver.findElement(By.xpath("//input[@id='user_name']")).sendKeys("admin");
 		driver.findElement(By.xpath("//input[@id='user_password']")).sendKeys("03-xMN$nqSmU");
+		WebElement login = driver.findElement(By.xpath("//button[text()='Log in']"));
+		login.click();
+		driver.quit();
+	}
+	
+	@Test(priority = 2)
+	public void runBuildSeeni() {
+
+		ChromeDriver driver = new ChromeDriver();
+		//1. Launch ServiceNow application
+		driver.get("https://dev181504.service-now.com/");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+        //2. Login with valid credentials username as admin and password
+		driver.findElement(By.xpath("//input[@id='user_name']")).sendKeys("admin");
+		driver.findElement(By.xpath("//input[@id='user_password']")).sendKeys("F/*jyM6Q6pHh");
 		WebElement login = driver.findElement(By.xpath("//button[text()='Log in']"));
 		login.click();
 		driver.quit();
